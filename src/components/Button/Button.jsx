04b3dd67552ef9fa016bdff900232ed781b87ae8
Rail Sabirov <rail.sabirov@gmail.com>
2023-10-15 08:42:11 -0400

    useState on Button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,12 +1,24 @@
 import './Button.css';
+import { useState } from 'react';
 
 function Button() {
+	// Хук для отслеживания состояния, первоначальное значение 'Save'
+	const [buttonText, setButtonText] = useState('Save');
+
+	// Отработка клика на кнопку, меняем название кнопки
 	const clicked = () => {
+		if (buttonText == 'Save') {
+			setButtonText('Close');
+		
+		} else {
+			setButtonText('Save');
+		}
+		
 		console.log('Button is was clicked!');
 	};
 
 	return (
-		<button onClick={ clicked }  className='button accent'>Button</button>
+		<button onClick={ clicked }  className='button accent'>{ buttonText }</button>
 	);
 }
 
