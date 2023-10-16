f45b33f6b412cb358d4fca31dbb9d7f90b7101a7
Rail Sabirov <rail.sabirov@gmail.com>
2023-10-15 20:02:11 -0400

    Add components: Header, JournlaList, JournalAddButton and layouts

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import './Header.css';
+
+const Header = () => {
+	return (
+		<div className="header">Header component</div>
+	);
+};
+
+export default Header;
---
+++ src/components/JournalAddButton/JournalAddButton.jsx
@@ -0,0 +1,9 @@
+import './JournalAddButton.css';
+
+const JournalAddButton = () => {
+	return (
+		<div className="journal-add-button">JournalAddButton component</div>	
+	);
+};
+
+export default JournalAddButton;
