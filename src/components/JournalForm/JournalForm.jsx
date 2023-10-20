74a0090d977d1ebc468cb29844c089189d4a8b15
Rail' Sabirov <rails@fivestarlhcsa.com>
2023-10-20 15:24:22 -0400

    Stylization invalidate form fields with tag style

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,7 +1,16 @@
 import './JournalForm.css';
 import Button from '../Button/Button';
+import { useState } from 'react';
 
 function JournalForm({ addItem }) {
+
+    // Статус для валидации формы, с первоначальными параметрами
+    const [formValidState, setFormValidState] = useState({
+        title: true,
+        post: true,
+        date: true
+    });
+
     const addJournalItem = (event) => {
         event.preventDefault();
 
@@ -11,16 +20,59 @@ function JournalForm({ addItem }) {
         // Получаем все значения из формы в виде объекта
         const formProps = Object.fromEntries(formData);
         
+        // Валидация полей формы
+        let isFormValid = true;
+
+        if (!formProps.title?.trim().length) {
+            setFormValidState(state => ({ ...state, title: false })); 
+            isFormValid = false;
+        
+        } else {
+            setFormValidState(state => ({ ...state, title: true }));
+        }
+
+        if (!formProps.date) {
+            setFormValidState(state => ({ ...state, date: false })); 
+            isFormValid = false;
+        
+        } else {
+            setFormValidState(state => ({ ...state, date: true }));
+        }
+
+        if (!formProps.post?.trim().length) {
+            setFormValidState(state => ({ ...state, post: false })); 
+            isFormValid = false;
+        
+        } else {
+            setFormValidState(state => ({ ...state, post: true }));
+        }
+
+        // Если форма не валидна не добавляем ничего
+        if (!isFormValid) {
+            return;
+        }
+
         // Вызываем прокинутую функцию для добавления данных
         addItem(formProps);
     };
 
     return (
         <form className='journal-form' onSubmit={addJournalItem}>
-            <input type="text" name="title" placeholder='Enter title'/>
-            <input type="date" name="date"/>
-            <input type="text" name="tag" placeholder='Enter tag'/>
-            <textarea name="post" id="" cols="30" rows="10" placeholder='Enter post'></textarea>
+            <input type="text" 
+                name="title" 
+                style={{border: formValidState.title ? undefined : '3px solid red'}}
+                placeholder='Enter title'/>
+            <input type="date" 
+                style={{border: formValidState.date ? undefined : '3px solid red'}}
+                name="date"/>
+            <input type="text" 
+                name="tag" 
+                placeholder='Enter tag'/>
+            <textarea name="post" 
+                cols="30" 
+                rows="10" 
+                style={{border: formValidState.post ? undefined : '3px solid red'}}
+                placeholder='Enter post'></textarea>
             <Button text="Save" />
         </form>
     );
