9aef49b8509eac8b8dabfac68b8e57a89cbc8228
Rail' Sabirov <rails@fivestarlhcsa.com>
2023-11-07 09:37:02 -0500

    Use useCallback for caching functions

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,16 +2,16 @@ import Button from '../Button/Button';
 import Logo from '../Logo/Logo';
 import SelectUser from '../SelectUser/SelectUser';
 import styles from './Header.module.css';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 const logos = ['./logo.svg', './vite.svg'];
 
 const Header = () => {
 	// Состояние для индекса логотипа
 	const [logoIndex, setLogoIndex] = useState(0);
-	const toggleLogo = () => {
+	const toggleLogo = useCallback( () => {
 		setLogoIndex(state => Number(!state));
-	}
+	}, []);
 
 	console.log('--Header--');
 
